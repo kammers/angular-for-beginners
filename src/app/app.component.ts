76a1751549a8b05bbeeacc70c4b83410c8a81295
cameronin <marcokammer59@gmail.com>
2021-03-02 14:16:09 +0200

    3-services

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,36 +1,51 @@
-import {
-  AfterViewInit,
-  Component,
-  ElementRef,
-  OnInit,
-  QueryList,
-  ViewChild,
-  ViewChildren,
-} from "@angular/core";
-import { COURSES } from "../db-data";
-import { Course } from "./model/course";
-import { CourseCardComponent } from "./course-card/course-card.component";
-import { HighlightedDirective } from "./directives/highlighted.directive";
-import { Observable } from "rxjs";
-import { CoursesService } from "./services/courses.service";
+import {ChangeDetectionStrategy, ChangeDetectorRef, Component, DoCheck, Inject, Injector, OnInit} from '@angular/core';
+import {Course} from './model/course';
+import {Observable} from 'rxjs';
+import {AppConfig, CONFIG_TOKEN} from './config';
+import {COURSES} from '../db-data';
+import {CoursesService} from './courses/courses.service';
+import {createCustomElement} from '@angular/elements';
+import {CourseTitleComponent} from './course-title/course-title.component';
+
 
 @Component({
-  selector: "app-root",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"],
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
-  courses$: Observable<Course[]>;
 
-  constructor(private coursesService: CoursesService) {}
+    courses: Course[] = COURSES;
 
-  ngOnInit() {
-    this.courses$ = this.coursesService.loadCourses();
-  }
+    coursesTotal = this.courses.length;
 
-  save(course: Course) {
-    this.coursesService
-      .saveCourse(course)
-      .subscribe(() => console.log("Course Saved!"));
-  }
+    constructor(
+        private coursesService: CoursesService,
+        @Inject(CONFIG_TOKEN) private config: AppConfig,
+        private injector: Injector) {
+
+    }
+
+    ngOnInit() {
+
+        const htmlElement = createCustomElement(CourseTitleComponent, {injector:this.injector});
+
+        customElements.define('course-title', htmlElement);
+
+    }
+
+    onEditCourse() {
+
+            this.courses[1].category = 'ADVANCED';
+
+    }
+
+    save(course: Course) {
+        this.coursesService.saveCourse(course)
+            .subscribe(
+                () => console.log('Course Saved!')
+            );
+    }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,27 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {CUSTOM_ELEMENTS_SCHEMA, NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { CourseCardComponent } from './course-card/course-card.component';
-import { CourseImageComponent } from './course-image/course-image.component';
-import { HighlightedDirective } from './directives/highlighted.directive';
-import { NgxUnlessDirective } from './directives/ngx-unless.directive';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {HttpClientModule} from '@angular/common/http';
+import {CoursesModule} from './courses/courses.module';
+import {CourseTitleComponent} from './course-title/course-title.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    CourseCardComponent,
-    CourseImageComponent,
-    HighlightedDirective,
-    NgxUnlessDirective
-  ],
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    HttpClientModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        CourseTitleComponent
+    ],
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        HttpClientModule,
+        CoursesModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
+    entryComponents: [CourseTitleComponent]
 })
-export class AppModule { }
+export class AppModule {
+
+}
